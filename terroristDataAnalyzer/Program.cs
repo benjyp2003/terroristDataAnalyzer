b9cebeb90cd2e9b253b4a7155ab0f7b441f00b53
benjyp2003 <benjypfeffer@gmail.com>
2025-05-15 12:45:13 +0300

    Add project files.

---
+++ terroristDataAnalyzer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace terroristDataAnalyzer
+{
+    internal class Program
+    {
+        
+
+
+    }
+}
+
